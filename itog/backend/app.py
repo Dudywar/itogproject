3ecf939c9530a53e7b0dd4451669b5460d9a8631
Dudywar <arseniy03052008@gmail.com>
2024-10-18 22:24:16 +0300

    Добавление бд для заказов

--- itog/backend/app.py
+++ itog/backend/app.py
@@ -1,5 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials, db
+
+databaseURL = 'https://zakazedi-3c538-default-rtdb.europe-west1.firebasedatabase.app/'
+
+cred = credentials.Certificate("zakazedi-3c538-firebase-adminsdk-xe6q5-273c0f0e8f.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': databaseURL
+})
+ref = db.reference("/")
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +18,12 @@ CORS(app)
 def order():
     if request.method == 'POST':
         data = request.get_json()
+        ref.set({
+            "id":
+                {
+                    "product": data.product,
+                }
+        })
         print(data)
         return jsonify({"message": "Заказ получен", "data": data}), 200
 
--- itog/frontend/js/index.js
+++ itog/frontend/js/index.js
@@ -2,6 +2,7 @@ const url = 'http://127.0.0.1:8000/';
 let pokupka = document.getElementById("pokupka");
 
 let zakaz = {
+    "id": "1",
     "product": "Суп"
 };
 
