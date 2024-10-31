85bfb7f2f8d398a7c53f59811b6e6b2b3faad753
Dudywar <107914280+Dudywar@users.noreply.github.com>
2024-10-31 13:14:41 +0300

    upload

---
+++ itogproject/itog/frontend/js/index.js
@@ -0,0 +1,29 @@
+const url = 'http://127.0.0.1:8000/';
+const socket = io('http://127.0.0.1:8000/');
+let pokupka = document.getElementById("pokupka");
+let stat = document.getElementById("status");
+let id = 0;
+
+socket.on('order_completed', function(zak) {
+    stat.innerHTML = `<h1>Заказ готов</h1>`;
+});
+
+pokupka.onclick = function() {
+    stat.innerHTML = `<h1>Заказ готовится</h1>`; 
+    console.log("заказ ушел");
+    id++;
+    console.log(id);
+    
+    let zakaz = {
+        "id": id,
+        "product": "Суп"
+    };
+
+    axios.post(url, zakaz)
+        .then(response => {
+            console.log(response.data);
+            //let res = response.data.data.product;
+            // alert(`${res} скоро будет готов`);
+        })
+        .catch(error => console.log(error));
+};
